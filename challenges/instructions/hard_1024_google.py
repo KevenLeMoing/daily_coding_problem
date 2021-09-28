3e2c67d2f6685de953eafe54d94dc3a2f0e77d28
lemoingk <KevenLeMoing@user-reglindiss-MacBook-Pro.local>
2021-09-28 14:39:42 +0200

    first version

---
+++ challenges/instructions/hard_1024_google.py
@@ -0,0 +1,22 @@
+"""
+Given an array of integers where every integer occurs three times except for one integer, which only occurs once, find and return the non-duplicated integer.
+For example, given [6, 1, 3, 3, 3, 6, 6], return 1. Given [13, 19, 13, 13], return 19.
+Do this in O(N) time and O(1) space.
+"""
+
+import itertools
+
+
+def my_func(my_list: list) -> int:
+    for a, b in itertools.combinations(my_list, 2):
+        if a != b:
+            if my_list.count(a) == 3:
+                continue
+        else:
+            continue
+
+
+def my_func_2(my_list: list) -> int:
+    for x in my_list:
+        if my_list.count(x) == 1:
+            return x
---
+++ challenges/tests/test_hard_1024_google.py
@@ -0,0 +1,6 @@
+from challenges.instructions.hard_1024_google import my_func_2
+
+
+def test_my_func_2():
+    assert my_func_2([13, 19, 13, 13]) == 19
+    assert my_func_2([6, 1, 3, 3, 3, 6, 6]) == 1
